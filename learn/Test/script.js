8c5b7e8d1a863eaeba7efb0e5bf3b3f552ed264e
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-01-12 07:05:58 -0600

    made slight changes

--- learn/Test/script.js
+++ learn/Test/script.js
@@ -15,51 +15,51 @@
 // console.log(height);
 
 // Variables
-let greeting = "Hello";
-let counter = 100;
-console.log(greeting + " " + counter + "!");
-const greet = "Hello!";
-// greet = "Hi!"
+// let greeting = "Hello";
+// let counter = 100;
+// console.log(greeting + " " + counter + "!");
+// const greet = "Hello!";
+// // greet = "Hi!"
 
-// Block
-let count = 0;
-console.log(count);  // -> 0
-{
-  let count = 5;
-  console.log(count);  // -> 5
-}
+// // Block
+// let count = 0;
+// console.log(count);  // -> 0
+// {
+//   let count = 5;
+//   console.log(count);  // -> 5
+// }
 
-// let v var
-var  height  =  180;
-{
-         var  weight  =  70;
-         console.log(height);  //  ->  180
-         console.log(weight);  //  ->  70	
-}
-console.log(height);  //  ->  180
-console.log(weight);  //  ->  70
+// // let v var
+// var  height  =  180;
+// {
+//          var  weight  =  70;
+//          console.log(height);  //  ->  180
+//          console.log(weight);  //  ->  70	
+// }
+// console.log(height);  //  ->  180
+// console.log(weight);  //  ->  70
 
 
-// function and var 
-var  globalGreeting  =  "Good  ";
+// // function and var 
+// var  globalGreeting  =  "Good  ";
    
-function  testFunction()  {
-         var  localGreeting  =  "Morning  ";    
-         console.log("function:");
-         console.log(globalGreeting);
-         console.log(localGreeting);
-}
+// function  testFunction()  {
+//          var  localGreeting  =  "Morning  ";    
+//          console.log("function:");
+//          console.log(globalGreeting);
+//          console.log(localGreeting);
+// }
    
-testFunction();
+// testFunction();
    
-console.log("main  program:");
-console.log(globalGreeting);
-// console.log(localGreeting);  //  ->  Uncaught  ReferenceError:  localGreeting  is  not  defined
+// console.log("main  program:");
+// console.log(globalGreeting);
+// // console.log(localGreeting);  //  ->  Uncaught  ReferenceError:  localGreeting  is  not  defined
 
-var  height  =  180;
-console.log(height);    //  ->  180
-console.log(weight);    //  ->  Uncaught  ReferenceError:  weight  is  not  defined
+// var  height  =  180;
+// console.log(height);    //  ->  180
+// console.log(weight);    //  ->  Uncaught  ReferenceError:  weight  is  not  defined
 
-var  height  =  180;
-console.log(height);    //  ->  180
-console.log(weight);    //  ->  Uncaught  ReferenceError:  weight  is  not  defined
+// var  height  =  180;
+// console.log(height);    //  ->  180
+// console.log(weight);    //  ->  Uncaught  ReferenceError:  weight  is  not  defined
