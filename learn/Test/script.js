977e062c35e3e9b3e89ab4df6ef0182fb3fd0186
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-01-12 06:39:52 -0600

    made slight changes

--- learn/Test/script.js
+++ learn/Test/script.js
@@ -9,7 +9,32 @@
 
 
 // We can ensure we are using modern javascript standards by adding "use strict" at the beginning of the script (we define variables before we use em)
-"use strict";
-let height = 180;
-height = "change height"
-console.log(height);
+// "use strict";
+// let height = 180;
+// height = "change height";
+// console.log(height);
+
+// Variables
+let greeting = "Hello";
+let counter = 100;
+console.log(greeting + " " + counter + "!");
+const greet = "Hello!";
+// greet = "Hi!"
+
+// Block
+let count = 0;
+console.log(count);  // -> 0
+{
+  let count = 5;
+  console.log(count);  // -> 5
+}
+
+// let v var
+var  height  =  180;
+{
+         var  weight  =  70;
+         console.log(height);  //  ->  180
+         console.log(weight);  //  ->  70	
+}
+console.log(height);  //  ->  180
+console.log(weight);  //  ->  70
