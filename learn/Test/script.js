ad3a3e1cd22f3041af6cff13c4209a24b4864d64
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-01-12 06:42:35 -0600

    made slight changes

--- learn/Test/script.js
+++ learn/Test/script.js
@@ -38,3 +38,20 @@ var  height  =  180;
 }
 console.log(height);  //  ->  180
 console.log(weight);  //  ->  70
+
+
+// function and var 
+var  globalGreeting  =  "Good  ";
+   
+function  testFunction()  {
+         var  localGreeting  =  "Morning  ";    
+         console.log("function:");
+         console.log(globalGreeting);
+         console.log(localGreeting);
+}
+   
+testFunction();
+   
+console.log("main  program:");
+console.log(globalGreeting);
+console.log(localGreeting);  //  ->  Uncaught  ReferenceError:  localGreeting  is  not  defined
