d4918a08a8c238c8af540c381673283984d0557e
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-01-12 06:30:35 -0600

    made slight changes

--- learn/Test/script.js
+++ learn/Test/script.js
@@ -1,1 +1,15 @@
-console.log("Starting server")
+// console.log("Starting server")
+// let  height  =  180;
+// let  anotherHeight  =  height;
+// let  weight;
+// console.log(height);  //  ->  180
+// console.log(anotherHeight);  //  ->  180
+// weight  =  70;    
+// console.log(weight);  //  ->  70
+
+
+// We can ensure we are using modern javascript standards by adding "use strict" at the beginning of the script (we define variables before we use em)
+"use strict";
+let height = 180;
+height = "change height"
+console.log(height);
