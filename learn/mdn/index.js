ad2d63b3a1d2b63934a147669f7cc3b5b0740cb3
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-01-21 13:19:23 -0600

    added info on how to add printers

---
+++ learn/mdn/index.js
@@ -0,0 +1,15 @@
+const myObj = {
+    a:1,b:{c:2}}
+    function printMe(obj){console.log(obj.b.c)
+}
+printMe(myObj)
+
+const myObj2 = {
+    a: 1,
+    b: { c: 2 },
+  };
+  function printMe(obj) {
+    console.log(obj.b.c);
+}
+printMe(myObj2);
+  
