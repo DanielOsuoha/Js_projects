382f5adcf527c04284371fa54e9073cc95e6107a
Daniel Osuoha <dan646655@gmail.com>
2025-09-27 14:43:54 -0500

    slight changes

--- sockets/sockets_chat_example/index.js
+++ sockets/sockets_chat_example/index.js
@@ -2,16 +2,22 @@ import express from 'express';
 import {createServer} from 'node:http';
 import {fileURLToPath} from 'node:url';
 import {dirname, join} from 'node:path';
+import {Server} from 'socket.io';
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express();
 const server = createServer(app);
+const io = Server(server);
 
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello guys</h1>');
     res.sendFile(join(__dirname, 'index.html'));
 });
+
+io.on('connection', (server)=>{
+    console.log('a user connected')
+})
 
 server.listen(3000, () => {
     console.log('server running at http://localhost:3000');
