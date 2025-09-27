5c96e3a7f7dfa40d1167b0ef5cc6e92f6310c94e
Daniel Osuoha <dan646655@gmail.com>
2025-09-27 14:06:08 -0500

    slight addition

--- sockets/first/common.js
+++ sockets/first/common.js
@@ -1,1 +1,14 @@
-import {Server} from "socket.io"
+// import {Server} from "socket.io"
+import express from 'express';
+import {createServer} from 'node:http';
+
+const app = express();
+const server = createServer(app);
+
+app.get('/', (req, res) => {
+    res.send('<h1>First server</h1>');
+});
+
+server.listen(3000, ()=>{
+    console.log('The app is running at http://localhost:3000');
+});
