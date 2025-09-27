18f2e739bb39d7acc46ff059fb767605ef997ce0
Daniel Osuoha <dan646655@gmail.com>
2025-09-27 17:47:13 -0500

    make changes

--- sockets/sockets_chat_example/index.js
+++ sockets/sockets_chat_example/index.js
@@ -17,15 +17,17 @@ app.get('/', (req, res) => {
 
 
 io.on('connection', (socket)=>{
-    console.log('a user connected')
+    socket.on('chat message', (msg)=>{
+        // console.log('Message: ' + msg)
+        io.emit('chat message', msg);
+    });
+    
+    // console.log('a user connected')
     // socket.on('disconnect', ()=>{
     //     console.log('a user disconnected');
     // })
-    socket.on('chat message', (msg)=>{
-        console.log('Message: ' + msg)
-    })
-})
+});
 
 server.listen(3000, () => {
     console.log('server running at http://localhost:3000');
-})
+});
