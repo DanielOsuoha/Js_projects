589dab1180cbef59386e13cde74c907fe057a8a7
Daniel Osuoha <dan646655@gmail.com>
2025-09-27 14:14:31 -0500

    slight addition

--- sockets/sockets_chat_example/index.js
+++ sockets/sockets_chat_example/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import {createServer} from 'node:http';
+import {fileURLToPath} from 'node:url';
+import {dirname, join} from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express();
 const server = createServer(app);
 
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello guys</h1>');
+    // res.send('<h1>Hello guys</h1>');
+    res.sendFile(join(__dirname, 'index.html'));
 });
 
 server.listen(3000, () => {
